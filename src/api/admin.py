fde8d800c789fa4e9464d87395c48aa9051080f9
Branden Reilly <155998782+brandenreilly@users.noreply.github.com>
2024-08-26 15:20:25 -0400

    backend admin-view changes 8/26/24

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,7 @@
 import os
 from flask_admin import Admin
 from .models import db, User, Favorites
+from .views import UserAdmin, FavoritesAdmin
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -10,8 +11,10 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Favorites, db.session))
+    admin.add_view(UserAdmin(User, db.session))
+    #admin.add_view(ModelView(User, db.session))
+    admin.add_view(FavoritesAdmin(Favorites, db.session))
+    #admin.add_view(ModelView(Favorites, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,13 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_admin.contrib.sqla import ModelView
-from app import app, db
-from models import User, Favorites
+from .models import User, Favorites
+
+class UserAdmin(ModelView):
+    column_list = ('id', 'username', 'email', 'bio')
+    column_labels = dict(id='Id', username='Username', email='Email', bio='Bio')
+    column_filters = ('id', 'username', 'email', 'bio')
+
+class FavoritesAdmin(ModelView):
+    column_list = ('title', 'user')
+    column_labels = {'title': 'Title', 'user': 'User'}
+    column_filters = ('title', 'user')
